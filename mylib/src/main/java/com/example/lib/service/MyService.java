d38570e0e621c2c637a010b850969f29d50099b3
Tarik Chrouki <tchrouki@webbfontaine.com>
2017-08-14 15:04:35 +0200

    first commit

---
+++ mylib/src/main/java/com/example/lib/service/MyService.java
@@ -0,0 +1,9 @@
+package com.example.lib.service;
+
+/**
+ * @author Tarik Chrouki on 14/08/17.
+ */
+public interface MyService {
+
+    public String hello();
+}
---
+++ mylib/src/main/java/com/example/lib/service/impl/MyServiceImpl.java
@@ -0,0 +1,14 @@
+package com.example.lib.service.impl;
+
+import com.example.lib.service.MyService;
+
+/**
+ * @author Tarik Chrouki on 14/08/17.
+ */
+public class MyServiceImpl implements MyService {
+
+    @Override
+    public String hello() {
+        return "Hello word";
+    }
+}
